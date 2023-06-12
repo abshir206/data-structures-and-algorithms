fe466ac0910098c5bde6f42ea00c4b55fd1eaeaf
Abshir Ibrahim <abshir.ibrahim@outlook.com>
2023-06-11 22:08:26 -0700

    lab31

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,16 @@
-'use strict';
+function firstDuplicate(str){
+  let regex = /,|\.|\?|\!|\:|\;/gm;
+  let newStr = str.toLowerCase().replace(regex, '');
+  let map = {};
 
-class LinkedList {
-
+  let words = newStr.split(' ');
+  for(let i=0; i<words.length; i++){
+    if(map[words[i]]){
+      return words[i];
+    }
+    map[words[i]] = 1;
+  }
+  return 'No Matches';
 }
 
-module.exports = LinkedList;
+module.exports = {firstDuplicate};
